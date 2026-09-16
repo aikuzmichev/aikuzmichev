1e8a07debec0bfaf550ca50f46b745acaff5f0e6
Andrey Kuzmichev <136024979+aikuzmichev@users.noreply.github.com>
2026-09-17 00:18:25 +0300

    Update convert_usd_to_ruble.py

--- convert_usd_to_ruble.py
+++ convert_usd_to_ruble.py
@@ -3,14 +3,14 @@ import requests
 
 data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
 current_datetime = datetime.today().date()
-  
-  print()
-  print("This program converts US dollars to Russian ruble")
-  print ("USD exchange rate for today", current_datetime, ":", data['Valute']['USD']['Value'], "₽")
-  print() 
 
-  dollars = eval(input("Enter amount in dollars: "))
-  print() 
+print()
+print("This program converts US dollars to Russian ruble")
+print ("USD exchange rate for today", current_datetime, ":", data['Valute']['USD']['Value'], "₽")
+print() 
 
-  convert_to_ruble = dollars * data['Valute']['USD']['Value']
-  print("That is" , convert_to_ruble, "₽ today")
+dollars = eval(input("Enter amount in dollars: "))
+print() 
+
+convert_to_ruble = dollars * data['Valute']['USD']['Value']
+print("That is" , convert_to_ruble, "₽ today")
